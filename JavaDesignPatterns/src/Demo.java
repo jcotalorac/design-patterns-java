090b4dcf029ff0c8f04317d4e4746d2229473739
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-16 19:29:38 -0500

    Method to insert an entry in the journal

--- JavaDesignPatterns/src/Demo.java
+++ JavaDesignPatterns/src/Demo.java
@@ -7,4 +7,8 @@ public class Demo {
 class Journal {
     private final List<String> entries = new ArrayList<>();
     private static int count = 0;
+
+    public void addEntry(String text) {
+        entries.add("" + (count++) + ": " + text);
+    }
 }
