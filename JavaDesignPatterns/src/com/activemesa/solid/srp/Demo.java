5c31efbcc96bc1da6cb26f3b441333a6439ca984
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-16 23:37:07 -0500

    Using the new class to solve Single Responsibility Principle

--- JavaDesignPatterns/src/com/activemesa/solid/srp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/srp/Demo.java
@@ -2,22 +2,28 @@ package com.activemesa.solid.srp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Journal j = new Journal();
         j.addEntry("I cried today");
         j.addEntry("I ate a bug");
         System.out.println(j);
+
+        Persistence p = new Persistence();
+        String filename = "/tmp/journal.txt";
+        p.saveToFile(j, filename, true);
+        Runtime.getRuntime().exec("gnome-text-editor " + filename);
     }
 }
 
 class Persistence {
-   public void save(Journal journal, String filename, boolean overwrite) throws FileNotFoundException {
+   public void saveToFile(Journal journal, String filename, boolean overwrite) throws FileNotFoundException {
        if (overwrite || new File(filename).exists()) {
            try (PrintStream out = new PrintStream(filename)) {
                out.println(journal.toString());
