ab89cf974125a504ea3310fed872c1e7b286a44c
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 12:13:06 -0500

    Filtering products by size

--- JavaDesignPatterns/src/com/activemesa/solid/ocp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/ocp/Demo.java
@@ -32,4 +32,8 @@ class ProductFilter {
     public Stream<Product> filterByColor(List<Product> products, Color color) {
         return products.stream().filter(p -> p.color == color);
     }
+
+    public Stream<Product> filterBySize(List<Product> products, Size size) {
+        return products.stream().filter(p -> p.size == size);
+    }
 }
