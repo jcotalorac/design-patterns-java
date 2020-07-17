b821188976cbdc73483f7c026bb99aa0534f1492
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 11:45:47 -0500

    Product model which contains properties with values from enums Color and Size to filter by them

--- JavaDesignPatterns/src/com/activemesa/solid/ocp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/ocp/Demo.java
@@ -2,3 +2,25 @@ package com.activemesa.solid.ocp;
 
 public class Demo {
 }
+
+enum Color {
+    RED,
+    GREEN,
+    BLUE
+}
+
+enum Size {
+    SMALL, MEDIUM, LARGE, HUGE
+}
+
+class Product {
+    public String name;
+    public Color color;
+    public Size size;
+
+    public Product(String name, Color color, Size size) {
+        this.name = name;
+        this.color = color;
+        this.size = size;
+    }
+}
