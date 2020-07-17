b7fdd0dd7e4fb0430158f6e1db14d4c1b6dded33
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 16:36:43 -0500

    Extension of a new specification, combining two of them by mean of And operation, without modifying previous filters

--- JavaDesignPatterns/src/com/activemesa/solid/ocp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/ocp/Demo.java
@@ -85,6 +85,20 @@ class SizeSpecification implements Specification<Product> {
     }
 }
 
+class AndSpecification<T> implements Specification<T> {
+    private Specification<T> first, second;
+
+    public AndSpecification(Specification<T> first, Specification<T> second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean isSatisfied(T item) {
+        return first.isSatisfied(item) && second.isSatisfied(item);
+    }
+}
+
 class BetterProductsFilter implements Filter<Product> {
 
     @Override
