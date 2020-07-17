6d70e813c74c4a8ac9a8b41d2fe22d1739bf0076
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 17:46:22 -0500

    Rectangle class with attributes, getters, setters, constructor and string conversion overridden method

--- JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
@@ -1,4 +1,38 @@
 package com.activemesa.solid.lsp;
 
+class Rectangle{
+    protected int width;
+    protected int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
+
 public class Demo {
 }
