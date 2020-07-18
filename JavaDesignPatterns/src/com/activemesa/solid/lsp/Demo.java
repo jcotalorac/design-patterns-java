e14f2499a7db803d07b8ae3b811887b678c9b35d
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 22:29:31 -0500

    First approach could be the detection of a rectangle when it is a square

--- JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
@@ -39,6 +39,10 @@ class Rectangle{
                 ", height=" + height +
                 '}';
     }
+
+    public boolean isSquare() {
+        return width == height;
+    }
 }
 
 class Square extends Rectangle{
