f12c65ebc536af31d2b8fce0fad7301adf56e324
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 22:39:04 -0500

    With help of factory pattern, her is the method to create squares, seen as a rectangle with same width and height.

--- JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
@@ -50,6 +50,10 @@ class RectangleFactory {
     public static Rectangle newRectangle(int width, int height) {
         return new Rectangle(width, height);
     }
+
+    public static Rectangle newSquare(int size) {
+        return new Rectangle(size, size);
+    }
 }
 
 class Square extends Rectangle{
