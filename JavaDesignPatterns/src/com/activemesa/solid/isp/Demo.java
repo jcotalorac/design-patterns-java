801b70578eca0638fb1bf5b28212f777571166ef
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 23:51:34 -0500

    With previous segregation of interfaces, it is possible to define new classes that take just the appropriate operations to be defined and implemented.

--- JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
@@ -59,5 +59,17 @@ class JustAPrinter implements Printer {
     }
 }
 
+class Photocopier implements Printer, Scanner {
+    @Override
+    public void print(Document document) {
+
+    }
+
+    @Override
+    public void scan(Document document) {
+
+    }
+}
+
 public class Demo {
 }
