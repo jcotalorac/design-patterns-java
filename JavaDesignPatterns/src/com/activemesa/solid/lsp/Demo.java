8a16713dd6c98e92bba2daa2f7ec135103577258
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 20:31:14 -0500

    Wrong output if the method is used by a subclass object

--- JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/lsp/Demo.java
@@ -72,5 +72,9 @@ public class Demo {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
         useIt(rectangle);
+
+        Rectangle square = new Square();
+        square.setWidth(5);
+        useIt(square);
     }
 }
