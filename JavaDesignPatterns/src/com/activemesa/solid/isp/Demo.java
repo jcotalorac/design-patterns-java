937454462f3d9742effc740018f0e96b0ad00782
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 23:12:14 -0500

    Representing documents and an interface to stands for Machine operations with documents

--- JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
@@ -1,4 +1,12 @@
 package com.activemesa.solid.isp;
 
+class Document {}
+
+interface Machine {
+    void print(Document document);
+    void fax(Document document);
+    void scan(Document document);
+}
+
 public class Demo {
 }
