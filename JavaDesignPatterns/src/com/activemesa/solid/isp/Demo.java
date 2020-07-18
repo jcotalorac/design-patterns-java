949598c7a8321f5c19934b62912e40b68c3f281c
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-17 23:14:25 -0500

    A common representation of a modern printer which is able to handle all operations with documents

--- JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
@@ -8,5 +8,23 @@ interface Machine {
     void scan(Document document);
 }
 
+class MultifunctionPrinter implements Machine {
+
+    @Override
+    public void print(Document document) {
+
+    }
+
+    @Override
+    public void fax(Document document) {
+
+    }
+
+    @Override
+    public void scan(Document document) {
+
+    }
+}
+
 public class Demo {
 }
