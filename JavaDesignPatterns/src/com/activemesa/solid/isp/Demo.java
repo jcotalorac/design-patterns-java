d2b1af4cb4f29aa0b37f86b34f8a61d9b18bd945
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-18 00:01:27 -0500

    Applying decorator design pattern

--- JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/isp/Demo.java
@@ -74,14 +74,23 @@ class Photocopier implements Printer, Scanner {
 interface MultiFunctionDevice extends Printer, Scanner {}
 
 class MultiFunctionMachine implements MultiFunctionDevice {
+
+    private Printer printer;
+    private Scanner scanner;
+
+    public MultiFunctionMachine(Printer printer, Scanner scanner) {
+        this.printer = printer;
+        this.scanner = scanner;
+    }
+
     @Override
     public void print(Document document) {
-
+        printer.print(document);
     }
 
     @Override
     public void scan(Document document) {
-
+        scanner.scan(document);
     }
 }
 
