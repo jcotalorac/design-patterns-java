f45ef953591fd85d14238851a01737d6a9167f78
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-18 21:39:58 -0500

    Example of modeling a person and possible relationships

--- JavaDesignPatterns/src/com/activemesa/solid/dip/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/dip/Demo.java
@@ -1,4 +1,18 @@
 package com.activemesa.solid.dip;
 
+enum Relationship {
+    PARENT,
+    CHILD,
+    SIBLING
+}
+
+class Person {
+    public String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
 public class Demo {
 }
