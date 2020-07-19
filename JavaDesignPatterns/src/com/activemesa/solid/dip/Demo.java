b72276aec74e417ad50f3d6a665588b73b89d8e0
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-18 22:46:59 -0500

    Class to handle a research on the relationships, this case, getting the children of a person called John.

--- JavaDesignPatterns/src/com/activemesa/solid/dip/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/solid/dip/Demo.java
@@ -17,14 +17,32 @@ class Person {
     public Person(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
 
 class Relationships {
     private List<Triplet<Person, Relationship, Person>> relations = new ArrayList<>();
 
+    public List<Triplet<Person, Relationship, Person>> getRelations() {
+        return relations;
+    }
+
     public void addParentAndChild(Person parent, Person child) {
         relations.add(new Triplet<>(parent, Relationship.PARENT, child));
         relations.add(new Triplet<>(child, Relationship.CHILD, parent));
+    }
+}
+
+class Research {
+    public Research(Relationships relationships) {
+        relationships.getRelations().stream().filter(relationship -> relationship.getValue0().name.equals("John")
+                && relationship.getValue1() == Relationship.PARENT).forEach(JohnChildren -> System.out.println("John children: " + JohnChildren.getValue2()));
     }
 }
 
