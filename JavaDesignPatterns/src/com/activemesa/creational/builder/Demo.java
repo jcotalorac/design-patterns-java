816a114524df28d3fc21352209dff0cee7252ab7
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-20 17:27:04 -0500

    Recursive printing of nested html elements

--- JavaDesignPatterns/src/com/activemesa/creational/builder/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/builder/Demo.java
@@ -1,6 +1,7 @@
 package com.activemesa.creational.builder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 class HtmlElement {
     public String name, text;
@@ -14,6 +15,31 @@ class HtmlElement {
     public HtmlElement(String name, String text) {
         this.name = name;
         this.text = text;
+    }
+
+    private String toStringImpl(int indent) {
+        StringBuilder sb = new StringBuilder();
+        String i = String.join("", Collections.nCopies(indent * indentSize, " "));
+        sb.append(String.format("%s<%s>%s", i, name, newLine));
+
+        if (text != null && !text.isEmpty()) {
+            sb.append(String.join("", Collections.nCopies(indentSize * (indent + 1), " ")))
+            .append(text)
+            .append(newLine);
+        }
+
+        for (HtmlElement e :
+                elements) {
+            sb.append(e.toStringImpl(indent + 1));
+        }
+
+        sb.append(String.format("%s</%s>%s", i, name, newLine));
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toStringImpl(0);
     }
 }
 
