e25a278d1cd89be61b95d298f13150e5e2aae405
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-20 18:45:55 -0500

    Class example of what it is needed to build

--- JavaDesignPatterns/src/com/activemesa/creational/builder/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/builder/Demo.java
@@ -90,3 +90,16 @@ public class Demo {
         System.out.println(builder);
     }
 }
+
+class Person {
+    public String name;
+    public String position;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
