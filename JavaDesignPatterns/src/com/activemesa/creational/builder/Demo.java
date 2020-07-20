ca61d8241a255a600d6620eca5c5585bad3b01d2
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-20 13:03:03 -0500

    Simple example of string construction by concatenation

--- JavaDesignPatterns/src/com/activemesa/creational/builder/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/builder/Demo.java
@@ -1,4 +1,8 @@
 package com.activemesa.creational.builder;
 
 public class Demo {
+    public static void main(String[] args) {
+        String hello = "hello";
+        System.out.println("<p>" + hello + "</p>");
+    }
 }
