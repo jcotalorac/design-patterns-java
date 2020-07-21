f0674f53713c109418b61013f197156b2c8fd907
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-21 15:09:59 -0500

    Constructing a Point class with cartesian coordinates

--- JavaDesignPatterns/src/com/activemesa/creational/factories/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/factories/Demo.java
@@ -1,4 +1,13 @@
 package com.activemesa.creational.factories;
 
+class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Demo {
 }
