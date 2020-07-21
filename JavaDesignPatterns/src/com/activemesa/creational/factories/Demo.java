b3b9f551c17b8bdb9a173507a82454dad262b8cc
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-21 15:25:27 -0500

    Creating an extra enum parameter in existent constructor to handle logic for cartesian and polar coordinates. The construction process here becomes more complicated and less readable (parameters change their meaning depending on system coordinate stated at input).

--- JavaDesignPatterns/src/com/activemesa/creational/factories/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/factories/Demo.java
@@ -1,14 +1,24 @@
 package com.activemesa.creational.factories;
 
+enum CoordinateSystem {
+    CARTESIAN,
+    POLAR
+}
+
 class Point {
     private double x, y;
 
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Point(double rho, double theta) {
+    public Point(double a, double b, CoordinateSystem cs) {
+        switch (cs) {
+            case CARTESIAN:
+                this.x = a;
+                this.y = b;
+                break;
+            case POLAR:
+                this.x = a * Math.cos(b);
+                this.y = a * Math.sin(b);
+                break;
+        }
     }
 }
 
