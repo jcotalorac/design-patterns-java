f675764de01a7f9266cb0546f796eb01ae9ec090
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-20 21:17:04 -0500

    Example domain Person class with two types of information to handle, personal and labor information

--- JavaDesignPatterns/src/com/activemesa/creational/builder/facets/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/builder/facets/Demo.java
@@ -1,4 +1,11 @@
 package com.activemesa.creational.builder.facets;
 
+class Person {
+    public String streetAddress, postcode, city;
+
+    public String companyName, position;
+    public int annualIncome;
+}
+
 public class Demo {
 }
