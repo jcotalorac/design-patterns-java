0dd92b1fa2d0e656e29cf37039edfa22c27f22e9
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-21 16:43:39 -0500

    One way to solve this is by mean of nesting the factory class inside the produced class Point.

--- JavaDesignPatterns/src/com/activemesa/creational/factories/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/factories/Demo.java
@@ -7,22 +7,24 @@ class Point {
         this.x = x;
         this.y = y;
     }
+
+    static class PointFactory {
+
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
+
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.cos(theta),
+                    rho * Math.sin(theta));
+        }
+    }
 }
 
-class PointFactory {
 
-    public static Point newCartesianPoint(double x, double y) {
-        return new Point(x, y);
-    }
-
-    public static Point newPolarPoint(double rho, double theta) {
-        return new Point(rho * Math.cos(theta),
-                rho * Math.sin(theta));
-    }
-}
 
 public class Demo {
     public static void main(String[] args) {
-        Point point = Point.newCartesianPoint(2, 3);
+        Point point = Point.PointFactory.newCartesianPoint(2, 3);
     }
 }
