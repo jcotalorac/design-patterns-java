b39de0c6f98a2013c47e984340c737fa7b574532
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-20 21:17:56 -0500

    String implementation of Person

--- JavaDesignPatterns/src/com/activemesa/creational/builder/facets/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/builder/facets/Demo.java
@@ -5,6 +5,18 @@ class Person {
 
     public String companyName, position;
     public int annualIncome;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", city='" + city + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", position='" + position + '\'' +
+                ", annualIncome=" + annualIncome +
+                '}';
+    }
 }
 
 public class Demo {
