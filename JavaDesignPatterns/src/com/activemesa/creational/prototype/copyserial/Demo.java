1b48aca533cc563eabc88946d222bd95ace439b9
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 17:36:58 -0500

    A class marked with Serialization interface

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/copyserial/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/copyserial/Demo.java
@@ -1,4 +1,24 @@
 package com.activemesa.creational.prototype.copyserial;
 
+import java.io.Serializable;
+
+class Foo implements Serializable {
+    public int stuff;
+    public String whatever;
+
+    public Foo(int stuff, String whatever) {
+        this.stuff = stuff;
+        this.whatever = whatever;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" +
+                "stuff=" + stuff +
+                ", whatever='" + whatever + '\'' +
+                '}';
+    }
+}
+
 public class Demo {
 }
