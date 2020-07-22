35ece63cb94b70ff7efdfd9b893f54918c803857
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-21 21:59:55 -0500

    Exercise solved. Factory implemented to return consecutive Person id

--- JavaDesignPatterns/src/com/activemesa/creational/factories/Exercise.java
+++ JavaDesignPatterns/src/com/activemesa/creational/factories/Exercise.java
@@ -3,6 +3,8 @@ package com.activemesa.creational.factories;
 
 class Person
 {
+    public static int count = 0;
+
     public int id;
     public String name;
 
@@ -11,15 +13,31 @@ class Person
         this.id = id;
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
 
 class PersonFactory
 {
     public Person createPerson(String name)
     {
-        // todo
+        return new Person(Person.count++, name);
     }
 }
 
 public class Exercise {
+    public static void main(String[] args) {
+        PersonFactory personFactory = new PersonFactory();
+        Person person0 = personFactory.createPerson("Person0");
+        Person person1 = personFactory.createPerson("Person1");
+
+        System.out.println(person0);
+        System.out.println(person1);
+    }
 }
