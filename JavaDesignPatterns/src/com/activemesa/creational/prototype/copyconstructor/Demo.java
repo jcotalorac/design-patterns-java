0e3781a57f74929e05999abfb8f330d0384f73b0
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 17:08:53 -0500

    With the use of copy constructor, it is possible to have a deep copy without the need to use clone, which is not recommended

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
@@ -47,4 +47,15 @@ class Employee {
 }
 
 public class Demo {
+    public static void main(String[] args) {
+        Employee john = new Employee("John", new Address(
+                "123 London Road", "London", "UK"
+        ));
+
+        Employee chris = new Employee(john);
+
+        chris.name = "Chris";
+        System.out.println(john);
+        System.out.println(chris);
+    }
 }
