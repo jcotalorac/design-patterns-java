e724437a5ddc75a0a0cd0c2ece54e11d949f3330
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-21 21:50:23 -0500

    Exercise to solve

---
+++ JavaDesignPatterns/src/com/activemesa/creational/factories/Exercise.java
@@ -0,0 +1,25 @@
+package com.activemesa.creational.factories;
+
+
+class Person
+{
+    public int id;
+    public String name;
+
+    public Person(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class PersonFactory
+{
+    public Person createPerson(String name)
+    {
+        // todo
+    }
+}
+
+public class Exercise {
+}
