9998bdeedc2377945d0ba9058a4ae65b7a9f701b
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 10:31:58 -0500

    A typical class to maintain an address

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/Demo.java
@@ -1,4 +1,22 @@
 package com.activemesa.creational.prototype;
 
+class Address {
+    public String streetName;
+    public int houseNumber;
+
+    public Address(String streetName, int houseNumber) {
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetName='" + streetName + '\'' +
+                ", houseNumber=" + houseNumber +
+                '}';
+    }
+}
+
 public class Demo {
 }
