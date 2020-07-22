84cc51dc0dcf203f4a6bbfbe4d7b7e5d4b57fa10
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 16:55:57 -0500

    Employee class with its generated constructor and its string conversion method

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
@@ -23,5 +23,23 @@ class Address {
     }
 }
 
+class Employee {
+    public String name;
+    public Address address;
+
+    public Employee(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
+
 public class Demo {
 }
