34e35192b8e0934ad2675fedf6973710607d41f2
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 18:22:28 -0500

    Exercise solved

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/Exercise.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/Exercise.java
@@ -9,6 +9,18 @@ class Point
         this.x = x;
         this.y = y;
     }
+
+    public Point(Point p) {
+        this(p.x, p.y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
 
 class Line
@@ -23,9 +35,30 @@ class Line
 
     public Line deepCopy()
     {
-        // todo
+        return new Line(new Point(this.start),
+                new Point(this.end));
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "start=" + start +
+                ", end=" + end +
+                '}';
     }
 }
 
 public class Exercise {
+    public static void main(String[] args) {
+        Line line1 = new Line(new Point(1,2),
+                new Point(3, 4));
+        Line line2 = line1.deepCopy();
+        line2.start.x = 5;
+        line2.start.y = 6;
+        line2.end.x = 7;
+        line2.end.y = 8;
+
+        System.out.println(line1);
+        System.out.println(line2);
+    }
 }
