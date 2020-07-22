c5ca04f43cf1a09b76936489c68befbb57579cd8
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 17:01:13 -0500

    Copy constructor for Employee class

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
@@ -32,6 +32,11 @@ class Employee {
         this.address = address;
     }
 
+    public Employee(Employee other) {
+        name = other.name;
+        address = new Address(other.address);
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
