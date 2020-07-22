388149aef7409d32c8b285f7f1c2786475ced0e4
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 16:47:56 -0500

    Address class with generated constructor and string conversion method

--- JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/prototype/copyconstructor/Demo.java
@@ -1,4 +1,23 @@
 package com.activemesa.creational.prototype.copyconstructor;
 
+class Address {
+    public String streetAddress, city, country;
+
+    public Address(String streetAddress, String city, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
+
 public class Demo {
 }
