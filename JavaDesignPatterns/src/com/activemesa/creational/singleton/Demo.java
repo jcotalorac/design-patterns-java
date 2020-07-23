136b9c9cf308ececdf1bbb9064f6fe512f0d2fdf
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 21:18:49 -0500

    Instance variable and exposed methods to handle it

--- JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
@@ -3,11 +3,20 @@ package com.activemesa.creational.singleton;
 class BasicSingleton {
     private static final BasicSingleton instance = new BasicSingleton();
 
+    private int value = 0;
     private BasicSingleton() {
     }
 
     public static BasicSingleton getInstance() {
         return instance;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
 
