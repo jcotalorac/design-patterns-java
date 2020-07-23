b35a22a9ead260ba81be35cfa95217dccad6ad1e
Juan Carlos Otalora <jcotalorac@gmail.com>
2020-07-22 21:31:51 -0500

    Simplest approach of a singleton implementation

--- JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
@@ -22,6 +22,8 @@ class BasicSingleton {
 
 public class Demo {
     public static void main(String[] args) {
-
+        BasicSingleton singleton = BasicSingleton.getInstance();
+        singleton.setValue(123);
+        System.out.println(singleton.getValue());
     }
 }
