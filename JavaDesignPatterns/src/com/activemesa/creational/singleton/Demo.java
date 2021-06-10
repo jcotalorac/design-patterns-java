fb51754bc2efa580cc26fb151405876bd6d407a3
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-10 18:05:21 -0500

    Value to be persisted in singleton enum

--- JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
@@ -97,6 +97,16 @@ class InnerStaticSingleton {
 
 enum EnumBasedSingleton {
     INSTANCE;
+
+    private int value;
+
+    public int getValue() {
+      return value;
+    }
+
+    public void setValue(int value) {
+      this.value = value;
+    }
 }
 
 public class Demo {
