d50bc2838d878063c00284f947c3d6a94eae6456
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-14 22:22:33 -0500

    Solution of exercise. To detect a singleton, it's needed to evaluate if the reference object and object values are the same. Test examples are given (non and singleton)

--- JavaDesignPatterns/src/com/activemesa/creational/singleton/Exercise.java
+++ JavaDesignPatterns/src/com/activemesa/creational/singleton/Exercise.java
@@ -6,13 +6,19 @@ class SingletonTester {
 
     public static boolean isSingleton(Supplier<Object> func) {
 
+        Object obj1 = func.get();
+        Object obj2 = func.get();
+
+        return obj1 == obj2 && obj1.equals(obj2);
     }
 }
 
 public class Exercise {
 
     public static void main(String[] args) {
+        System.out.println(SingletonTester.isSingleton(() -> Integer.MAX_VALUE));
 
+        System.out.println(SingletonTester.isSingleton(() -> Runtime.getRuntime()));
     }
 
 }
