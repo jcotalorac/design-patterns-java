e984859b71d744aa2a3589e66d4f82f36c7a2f31
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-14 21:43:56 -0500

    Customized database implementation to work with personalized mock data to support a real unit test

--- JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/creational/singleton/Demo.java
@@ -248,6 +248,22 @@ class ConfigurableRecordFinder {
     }
 }
 
+class DummyDatabase implements Database {
+
+    private Dictionary<String, Integer> data = new Hashtable<>();
+
+    public DummyDatabase() {
+        data.put("alpha", 1);
+        data.put("beta", 2);
+        data.put("gamma", 3);
+    }
+
+    @Override
+    public int getPopulation(String name) {
+        return data.get(name);
+    }
+}
+
 class Tests {
 
     @Test
