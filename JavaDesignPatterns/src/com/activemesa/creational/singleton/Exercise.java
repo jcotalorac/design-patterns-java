a9f054261f803ba02a8b4187d388431bcd876c1e
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-14 22:04:59 -0500

    Proposed exercise to resolve the problem to determine when a factory method object return is a singleton

---
+++ JavaDesignPatterns/src/com/activemesa/creational/singleton/Exercise.java
@@ -0,0 +1,18 @@
+package com.activemesa.creational.singleton;
+
+import java.util.function.Supplier;
+
+class SingletonTester {
+
+    public static boolean isSingleton(Supplier<Object> func) {
+
+    }
+}
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+    }
+
+}
