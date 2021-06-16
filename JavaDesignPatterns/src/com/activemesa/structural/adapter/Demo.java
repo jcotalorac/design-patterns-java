9ffd8024b0887e8999a4cab9647c97918f9c5207
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-15 21:26:56 -0500

    Disposable method to draw points

--- JavaDesignPatterns/src/com/activemesa/structural/adapter/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/adapter/Demo.java
@@ -47,4 +47,8 @@ public class Demo {
 
     private final static List<VectorObject> vectorObjects = new ArrayList<>(Arrays.asList(new VectorRectangle(1,1,10,10),
             new VectorRectangle(3, 3, 6, 6)));
+
+    public static void drawPoint(Point P) {
+
+    }
 }
