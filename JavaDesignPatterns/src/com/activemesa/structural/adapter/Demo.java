f86d019f9818f2643b654f017d2202c0412097de
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-15 21:02:07 -0500

    Line class using start and end points

--- JavaDesignPatterns/src/com/activemesa/structural/adapter/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/adapter/Demo.java
@@ -17,5 +17,15 @@ class Point {
     }
 }
 
+class Line {
+
+    public Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
 public class Demo {
 }
