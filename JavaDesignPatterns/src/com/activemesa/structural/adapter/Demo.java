60d0d3358edd0cdbf0b4839e541f25467874ca7f
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-25 16:47:52 -0500

    Adjusting trajectory. The example should be working with any trajectory to produce 8 lines of 2 rectangles

--- JavaDesignPatterns/src/com/activemesa/structural/adapter/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/adapter/Demo.java
@@ -72,8 +72,8 @@ class VectorRectangle extends VectorObject {
     public VectorRectangle(int x, int y, int width, int height) {
         add(new Line(new Point(x, y), new Point(x + width, y)));
         add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
-        add(new Line(new Point(x + width, y + height), new Point(x, y + height)));
-        add(new Line(new Point(x, y + height), new Point(x, y)));
+        add(new Line(new Point(x, y), new Point(x, y + height)));
+        add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
     }
 }
 
