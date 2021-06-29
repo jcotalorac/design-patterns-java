65d0a641cd10508d3d440f16b1f141e59320dcdf
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-28 21:58:12 -0500

    Modeling a shape with an abstract class instead of a concrete class, allows to maintain an aggregation of Renderer instance replacing the need to inherit from shape and renderer by two and producing M x N types of M shapes with N renderers.

--- JavaDesignPatterns/src/com/activemesa/structural/bridge/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/bridge/Demo.java
@@ -22,5 +22,17 @@ class RasterRenderer implements Renderer {
     }
 }
 
+abstract class Shape {
+
+    protected Renderer renderer;
+
+    public Shape(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public abstract void draw();
+    public abstract void resize(float factor);
+}
+
 public class Demo {
 }
