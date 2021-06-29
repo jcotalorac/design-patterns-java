a82f6ef4417acd7fc0c9172b036cd93281a2c74d
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-06-28 21:48:16 -0500

    Instead of a base concrete class, with bridge pattern is preferred to have an interface with concrete implementations

--- JavaDesignPatterns/src/com/activemesa/structural/bridge/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/bridge/Demo.java
@@ -1,4 +1,26 @@
 package com.activemesa.structural.bridge;
 
+interface Renderer {
+    void renderCircle(float radius);
+}
+
+class VectorRenderer implements Renderer {
+
+    @Override
+    public void renderCircle(float radius) {
+        System.out.println("Drawing a circle of radius "
+        + radius);
+    }
+}
+
+class RasterRenderer implements Renderer {
+
+    @Override
+    public void renderCircle(float radius) {
+        System.out.println("Drawing pixels for a circle "
+                + "of radius " + radius);
+    }
+}
+
 public class Demo {
 }
