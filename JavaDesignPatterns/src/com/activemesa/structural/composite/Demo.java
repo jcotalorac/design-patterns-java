1418ed2134a236d55e3fcd99e57342db13fa6e69
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-11 12:23:36 -0500

    By default, a graphic object is a group of shapes.

--- JavaDesignPatterns/src/com/activemesa/structural/composite/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/composite/Demo.java
@@ -2,6 +2,15 @@ package com.activemesa.structural.composite;
 
 class GraphincObject {
 
+  protected String name = "Group";
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
 
 public class Demo {
