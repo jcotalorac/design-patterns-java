48d6d91310038dca90f20bf04b80eceefc995e9f
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-10 20:23:06 -0500

    Relocating classes to avoid duplications with Demo ones

--- JavaDesignPatterns/src/com/activemesa/structural/bridge/Exercise.java
+++ JavaDesignPatterns/src/com/activemesa/structural/bridge/Exercise.java
@@ -1,45 +1,47 @@
 package com.activemesa.structural.bridge;
 
-abstract class Shape
-{
-  public abstract String getName();
-}
-
-class Triangle extends Shape
-{
-  @Override
-  public String getName()
-  {
-    return "Triangle";
-  }
-}
-
-class Square extends Shape
-{
-  @Override
-  public String getName()
-  {
-    return "Square";
-  }
-}
-
-class VectorSquare extends Square
-{
-  @Override
-  public String toString()
-  {
-    return String.format("Drawing %s as lines", getName());
-  }
-}
-
-class RasterSquare extends Square
-{
-  @Override
-  public String toString()
-  {
-    return String.format("Drawing %s as pixels", getName());
-  }
-}
-
 public class Exercise {
+
+  abstract class Shape
+  {
+    public Renderer renderer;
+
+    public abstract String getName();
+  }
+
+  class Triangle extends Shape
+  {
+    @Override
+    public String getName()
+    {
+      return "Triangle";
+    }
+  }
+
+  class Square extends Shape
+  {
+    @Override
+    public String getName()
+    {
+      return "Square";
+    }
+  }
+
+  class VectorSquare extends Square
+  {
+    @Override
+    public String toString()
+    {
+      return String.format("Drawing %s as lines", getName());
+    }
+  }
+
+  class RasterSquare extends Square
+  {
+    @Override
+    public String toString()
+    {
+      return String.format("Drawing %s as pixels", getName());
+    }
+  }
 }
