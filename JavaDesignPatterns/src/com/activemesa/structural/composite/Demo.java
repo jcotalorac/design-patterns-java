61d717907c2ba764a2221e842daa110b1110376f
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-11 17:07:59 -0500

    Adding color property, default constructor and list to handle graphic object either as a single graphic or a group of graphics.

--- JavaDesignPatterns/src/com/activemesa/structural/composite/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/composite/Demo.java
@@ -1,8 +1,17 @@
 package com.activemesa.structural.composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class GraphicObject {
 
   protected String name = "Group";
+  public String color;
+  public List<GraphicObject> children = new ArrayList<>();
+
+  public GraphicObject() {
+
+  }
 
   public String getName() {
     return name;
