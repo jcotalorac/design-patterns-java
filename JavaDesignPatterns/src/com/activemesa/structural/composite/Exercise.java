7d30fc4dd7da07fc77235c8928de4a80666f7330
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-12 17:38:38 -0500

    Solution of composite pattern exercise

--- JavaDesignPatterns/src/com/activemesa/structural/composite/Exercise.java
+++ JavaDesignPatterns/src/com/activemesa/structural/composite/Exercise.java
@@ -2,6 +2,11 @@ package com.activemesa.structural.composite;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 interface ValueContainer extends Iterable<Integer> {}
 
@@ -13,6 +18,21 @@ class SingleValue implements ValueContainer
   public SingleValue(int value)
   {
     this.value = value;
+  }
+
+  @Override
+  public Iterator<Integer> iterator() {
+    return Collections.singleton(value).iterator();
+  }
+
+  @Override
+  public void forEach(Consumer<? super Integer> action) {
+    action.accept(value);
+  }
+
+  @Override
+  public Spliterator<Integer> spliterator() {
+    return Collections.singleton(value).spliterator();
   }
 }
 
@@ -31,9 +51,28 @@ class MyList extends ArrayList<ValueContainer>
 
   public int sum()
   {
-    // todo
+    int sum = 0;
+    for (ValueContainer v : this) {
+      for (Integer i : v) {
+        sum += i;
+      }
+    }
+
+    return sum;
   }
 }
 
 public class Exercise {
+
+  public static void main(String[] args) {
+    SingleValue singleValue = new SingleValue(2);
+    ManyValues manyValues = new ManyValues();
+    manyValues.add(6);
+    manyValues.add(7);
+
+    MyList myList = new MyList(List.of(singleValue));
+    System.out.println(myList.sum());
+    myList = new MyList(List.of(singleValue, manyValues));
+    System.out.println(myList.sum());
+  }
 }
