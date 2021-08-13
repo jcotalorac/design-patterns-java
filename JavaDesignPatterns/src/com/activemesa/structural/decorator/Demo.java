635808509d0ab07e6a3e9add22ee007c3f13d203
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-12 21:12:10 -0500

    Specifying decorated shape and added color feature

--- JavaDesignPatterns/src/com/activemesa/structural/decorator/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/decorator/Demo.java
@@ -365,6 +365,14 @@ class Square implements Shape {
 
 class ColoredShape implements Shape {
 
+  private Shape shape;
+  private String color;
+
+  public ColoredShape(Shape shape, String color) {
+    this.shape = shape;
+    this.color = color;
+  }
+
   @Override
   public String info() {
     return null;
