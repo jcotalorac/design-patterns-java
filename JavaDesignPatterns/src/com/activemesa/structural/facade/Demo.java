0d94e9d02820b34516b56fd767880ee7384c1f33
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-16 11:42:09 -0500

    Example of low level calls. User needs to construct all the dependencies to use a console

--- JavaDesignPatterns/src/com/activemesa/structural/facade/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/facade/Demo.java
@@ -66,4 +66,13 @@ class Console {
 }
 
 public class Demo {
+
+  public static void main(String[] args) {
+    Buffer buffer = new Buffer(30, 20);
+    Viewport viewport = new Viewport(buffer, 30, 20, 0, 0);
+
+    Console console = new Console(30, 20);
+    console.addViewport(viewport);
+    console.render();
+  }
 }
