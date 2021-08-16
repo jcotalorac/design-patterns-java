5b5e4cab86885639d291f8675b0ba1366235a9d3
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-16 11:12:27 -0500

    Representation of user graphic visible area

--- JavaDesignPatterns/src/com/activemesa/structural/facade/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/facade/Demo.java
@@ -15,5 +15,22 @@ class Buffer {
   }
 }
 
+class Viewport {
+
+  private final Buffer buffer;
+  private final int width;
+  private final int height;
+  private final int offsetX;
+  private final int offsetY;
+
+  public Viewport(Buffer buffer, int width, int height, int offsetX, int offsetY){
+    this.buffer = buffer;
+    this.width = width;
+    this.height = height;
+    this.offsetX = offsetX;
+    this.offsetY = offsetY;
+  }
+}
+
 public class Demo {
 }
