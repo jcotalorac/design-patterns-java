925bc8bc6e2106f828a946f5a23a9dfb87e291a2
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-16 10:49:56 -0500

    Construction of a buffer with height and width of line. Allocating array of characters in one dimension

--- JavaDesignPatterns/src/com/activemesa/structural/facade/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/facade/Demo.java
@@ -4,6 +4,11 @@ class Buffer {
 
   private char[] characters;
   private int lineWidth;
+
+  public Buffer(int lineHeight, int lineWidth) {
+    this.lineWidth = lineWidth;
+    characters = new char[lineWidth * lineHeight];
+  }
 }
 
 public class Demo {
