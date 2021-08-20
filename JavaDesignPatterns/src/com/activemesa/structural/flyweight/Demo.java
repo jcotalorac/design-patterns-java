12caf72df74ebcdb06b01cd2e54c589809248cae
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-20 16:28:13 -0500

    Simple approach to deal with user names, but it would derive in duplications of firsts names and lasts names

--- JavaDesignPatterns/src/com/activemesa/structural/flyweight/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/flyweight/Demo.java
@@ -1,4 +1,13 @@
 package com.activemesa.structural.flyweight;
 
+class User {
+
+  private String fullName;
+
+  public User(String fullName) {
+    this.fullName = fullName;
+  }
+}
+
 public class Demo {
 }
