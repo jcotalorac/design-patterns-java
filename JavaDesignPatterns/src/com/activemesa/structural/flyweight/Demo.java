a392c069b7030a52f6c9dd2d25e23083646ed3eb
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-20 19:38:49 -0500

    Text Range class is the applied flyweight pattern to save memory having ranges instead of individual positions to store

--- JavaDesignPatterns/src/com/activemesa/structural/flyweight/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/flyweight/Demo.java
@@ -126,5 +126,9 @@ public class Demo {
     FormattedText ft = new FormattedText("This is a brave new world");
     ft.capitalize(10, 15);
     System.out.println(ft);
+
+    BetterFormattedText bft = new BetterFormattedText("Make America Great Again");
+    bft.getRange(13, 18).capitalize = true;
+    System.out.println(bft);
   }
 }
