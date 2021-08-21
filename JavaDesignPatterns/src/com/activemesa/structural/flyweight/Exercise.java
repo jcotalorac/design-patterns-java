afe336dbcede7044e45d74dd34c21cfd7ef511ac
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-20 19:40:15 -0500

    Proposed exercise to solve for flyweight design pattern

---
+++ JavaDesignPatterns/src/com/activemesa/structural/flyweight/Exercise.java
@@ -0,0 +1,28 @@
+package com.activemesa.structural.flyweight;
+
+class Sentence
+{
+  public Sentence(String plainText)
+  {
+    // todo
+  }
+
+  public WordToken getWord(int index)
+  {
+    // todo
+  }
+
+  @Override
+  public String toString()
+  {
+    // todo
+  }
+
+  class WordToken
+  {
+    public boolean capitalize;
+  }
+}
+
+public class Exercise {
+}
