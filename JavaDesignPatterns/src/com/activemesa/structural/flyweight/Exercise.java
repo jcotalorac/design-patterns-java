335d565e712fe1eb2288b79aefbe168e4e47dfae
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-21 13:20:37 -0500

    Solution to flyweight design pattern exercise

--- JavaDesignPatterns/src/com/activemesa/structural/flyweight/Exercise.java
+++ JavaDesignPatterns/src/com/activemesa/structural/flyweight/Exercise.java
@@ -1,28 +1,68 @@
 package com.activemesa.structural.flyweight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Sentence
 {
+  private String plainText;
+  private List<WordToken> wordTokens = new ArrayList<>();
+
   public Sentence(String plainText)
   {
-    // todo
+    this.plainText = plainText;
   }
 
   public WordToken getWord(int index)
   {
-    // todo
+    WordToken wordToken = new WordToken(index);
+    wordTokens.add(wordToken);
+
+    return wordToken;
   }
 
   @Override
   public String toString()
   {
-    // todo
+    StringBuilder sb = new StringBuilder();
+
+    String[] words = plainText.split(" ");
+
+    for (int i = 0; i < words.length; i++) {
+      String w = words[i];
+
+      for (WordToken wordToken : wordTokens) {
+        w = wordToken.existsWord(i) && wordToken.capitalize ? w.toUpperCase() : w;
+      }
+      sb.append(w).append(" ");
+    }
+
+    sb.deleteCharAt(sb.length() - 1);
+
+    return sb.toString();
   }
 
   class WordToken
   {
+    public int index;
     public boolean capitalize;
+
+    public WordToken(int index) {
+      this.index = index;
+    }
+
+    public boolean existsWord(int index) {
+      return this.index == index;
+    }
   }
 }
 
 public class Exercise {
+
+  public static void main(String[] args) {
+    Sentence sentence = new Sentence("hello world");
+    sentence.getWord(1).capitalize = true;
+    System.out.println(sentence);
+  }
+
 }
