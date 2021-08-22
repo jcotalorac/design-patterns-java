49d9c305272b05a5df33328793d377852af5259e
Juan Carlos Otalora <jcotalorac@gmail.com>
2021-08-21 21:21:16 -0500

    A car implementation of a drivable interface, with a drive required to construct one

--- JavaDesignPatterns/src/com/activemesa/structural/proxy/Demo.java
+++ JavaDesignPatterns/src/com/activemesa/structural/proxy/Demo.java
@@ -5,5 +5,28 @@ interface Drivable {
   void drive();
 }
 
+class Car implements Drivable {
+
+  protected Driver driver;
+
+  public Car(Driver driver) {
+    this.driver = driver;
+  }
+
+  @Override
+  public void drive() {
+    System.out.println("Car being driven");
+  }
+}
+
+class Driver {
+
+  public int age;
+
+  public Driver(int age) {
+    this.age = age;
+  }
+}
+
 public class Demo {
 }
